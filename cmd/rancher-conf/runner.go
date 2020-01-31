b5cbe4fef6ca612aa8b2b33a34a3b157ed87a769
Brian Dentino <brian.dentino@gmail.com>
2020-01-31 02:21:59 -0600

    Fix self service

--- cmd/rancher-conf/runner.go
+++ cmd/rancher-conf/runner.go
@@ -326,7 +326,7 @@ func (r *runner) createContext() (*TemplateContext, error) {
 			service.Stack.Services = append(service.Stack.Services, &service)
 		}
 
-		if s.UUID == metaSelf.ServiceUUID {
+		if s.StackNme == metaSelf.StackName && s.ServiceNme == metaSelf.ServiceName {
 			log.Debugf("Setting Self.Service to %s", s.Name)
 			self.Service = &service
 		}
